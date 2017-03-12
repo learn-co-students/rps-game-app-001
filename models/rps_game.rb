9be074fc53ae3d86d554d39ed82131aed08fdd0f
epang1 <epang1@gmail.com>
2017-03-12 16:34:54 -0400

    done

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,59 @@
 class RPSGame
+
+  attr_accessor :type
+
+  def initialize(type)
+    if type != :rock && type != :paper && type != :scissors
+      raise self.class::PlayTypeError
+    end
+
+    @type = type
+  end
+
+  class PlayTypeError < StandardError
+
+  end
+
+  def self.valid_play?(type)
+    if type == :rock || type == :paper || type == :scissors
+      true
+    else
+      false
+    end
+  end
+
+  def computer_play
+    @computer_play = [:scissors, :rock, :paper].sample
+  end
+
+  def won?
+    if @type == :rock && self.computer_play == :scissors
+      true
+    elsif @type == :scissors && self.computer_play == :paper
+      true
+    elsif @type == :paper && self.comptuer_play == :rock
+      true
+    else
+      false
+    end
+  end
+
+  def tied?
+    if @type == self.computer_play
+      true
+    else
+      false
+    end
+  end
+
+  def lost?
+    if self.won? == true || self.tied? == true
+      false
+    else
+      true
+    end
+  end
 end
+
+
+
